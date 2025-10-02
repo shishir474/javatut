949ebbaf845bebdfb86782090acabb907bbaefe8
shishir-klg <shishir.singh@kellogg.com>
2025-10-02 19:26:23 +0530

    method overloading

--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -7,6 +7,26 @@ public class JavaMethods {
     // protected: this method can be accesed by any class which exists in the same package or subclass in different package
     // default: this is the default access specifier used by java, if you dont mention anything. This method can only be accessed by classes in the same package.
 
+    // Method overloading
+    void getInvoice(){
 
+    }
 
+//    string getInvoice(){
+//
+//    }
+//    this is incorrect, because overloaded methods can only be differentiated based on arguments. Function name should be same, and arguments should be different).
+//    return type is not even considered
+
+    String getInvoice(string x){
+        // works fine
+    }
+    int getInvoice(int a){
+        // works fine
+    }
+    void getInvoice(int a, int b){
+        // works fine
+    }
+
+    // For method overloading only condition is same method name, different arguments. Return type is not even considered
 }
