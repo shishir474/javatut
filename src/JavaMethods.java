93472d392343206e2cbe1c86a5882ac3981807a4
shishir-klg <shishir.singh@kellogg.com>
2025-10-02 19:09:24 +0530

    access specifiers

---
+++ src/JavaMethods.java
@@ -0,0 +1,12 @@
+public class JavaMethods {
+    // package: is a collection of logical/similar classes
+
+    // access specifiers
+    // public: this method can be accessed in any class in any package
+    // private: this method can only be accessed inside the class in which it is defined
+    // protected: this method can be accesed by any class which exists in the same package or subclass in different package
+    // default: this is the default access specifier used by java, if you dont mention anything. This method can only be accessed by classes in the same package.
+
+
+
+}
