8942c170f0f6393cdf2cc31e2f6c7cbee28fb15c
shishir-klg <shishir.singh@kellogg.com>
2025-10-03 21:33:08 +0530

    garbage collection

--- src/JavaMemoryManagement.java
+++ src/JavaMemoryManagement.java
@@ -58,13 +58,24 @@ public class JavaMemoryManagement {
     7. Created another reference for personObj object.
     8. Created another reference for string literal. Since this literal already exists in string constant pool, only a reference was created and this reference again is stored on stack.
     9. Created String object stringObj. Space is allocated on heap(outside string pool) and its reference is stored in stringObj variable on stack.
-    10. closing bracket of memoryManagementTest() reached. Function execution complete. All the variables/references created as part of this invokation are deleted in LIFO order.
+    10. closing bracket of memoryManagementTest() reached. Function execution complete. All the variables/references created as part of this invocation are deleted in LIFO order.
         First reference for stringObj object is deleted. Then literal2 reference is deleted. Then person2 reference is deleted.
     11. closing bracket of main() method reached means its execution is also complete. All the variables/references created within main method on stack will be deleted in LIFO order.
 
 
-    NOTE: as you see once the method invokation is complete, only the references are deleted from stack memory. What about the actual objects that were created in the heap?
+    NOTE: as you see once the method invocation is complete, only the references are deleted from stack memory. What about the actual objects that were created in the heap?
     This will be taken care by GC(garbage collector).
+
+    GC runs periodically and deletes any object from heap that is no longer referenced. GC invocation is completely handled by JVM.
+
+    System.gc() --> using this command we can trigger GC. But its not guaranteed, since JVM controls when to trigger GC. If JVM sees the need, it will trigger GC. That is why this process is known as automatic memory management.
+    We dont have to worry about freeing up the memory. JVM automatically triggers the GC at periodic intervals which scans the heap and identifies objects that are not referenced and deletes them.
+
+    If heap memory is filling up fast, JVM will run the GC more frequently. If there's sufficient space, the freq of GC invocation will be reduced by JVM.
+
+
+
+
 
 
 
