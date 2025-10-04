1126c1ba7c43faeb63ddf7a7e3de573d96273104
shishir-klg <shishir.singh@kellogg.com>
2025-10-04 21:08:21 +0530

    nested class intro

---
+++ JavaClasses/src/NestedClass.java
@@ -0,0 +1,21 @@
+public class NestedClass {
+
+    Class within another class is called nested class.
+
+    When to use?
+    If you know that a class (A) will be used by only one another class (B), then instead of creating new file (A.java) for it, we can create nested class inside B itself.
+    It allows us to group logically related classes in one file.
+
+    Scope:
+    Its scope is same as of its outer class
+
+    Nested class is of 2 types
+        static nested class
+        non static nested class / inner class
+            a. member inner class
+            b. local inner class
+            c. anonymous inner class
+
+
+
+}
