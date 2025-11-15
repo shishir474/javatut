744106ecb8316b3f5687ce9ded5694731a5ef4ee
shishir474 <shishirsinghvaranasi@gmail.com>
2025-11-16 02:55:58 +0530

    why immutable classes needs to be final?

--- JavaClasses/src/Immutable.java
+++ JavaClasses/src/Immutable.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// final -- means we dont want any subclass to change its value. Declaring class as final -- means the class cannot be extended (so no subclass is allowed).
+// final -- means we dont want any subclass to override its behaviour and break its immutability.
+// Declaring class as final -- means the class cannot be extended (so no subclass is allowed).
 public final class Immutable {
     // Immutable -- means we cannot change the value of an object once it is created.
-    // declare the class as 'final', so that it cannot be extended.
+
+    // imp points:
+    // Declare the class as 'final', so that it cannot be extended.
     // Declare all class members as private, So that direct access is avoided.
     // And class members are initialized only once using constructor.
     // There shouldn't be setter methods, which is generally used to change values.
@@ -63,3 +66,56 @@ public class Main{
 // marking petNameList variable as final means that this variable will always point this list no matter what & it can never point to other memory reference.
 // if you return the original ref, your original list might get corrupted & thus in order to prevent that
 // & to maintain immutable behaviour(of the values of list, just to ensure that values dont change and we always get the same original version) we return the copy of the list
+
+// That's why it is recommended to always return the copy of the collection in the getter method.
+// Always remember, making list final just means -- its reference cannot be broken/changed. List modification is still possible.
+
+
+//Why declaring class final is mandatory, for immutability ?
+//    To make class immutabitle, we declare class as final, so that it cannot be extended.
+//
+//What is the harm if a class is allowed to be extended? How will it break immutability?
+
+        public class Money {
+            private final int amount;
+
+            public Money(int amount) {
+                this.amount = amount;
+            }
+
+            public int getAmount() {
+                return amount;
+            }
+        }
+        // If it's not final, someone can subclass it:
+
+        public class MutableMoney extends Money {
+
+            private int amount;
+
+            public MutableMoney(int amount) {
+                super(amount);
+                this.amount = amount;
+            }
+
+            @Override
+            public int getAmount() {
+                return amount;
+            }
+
+            public void setAmount(int amount) {
+                this.amount = amount;  // Mutability added
+            }
+        }
+
+//        You're right that the setter only changes MutableMoney.amount, not Money.amount.
+//
+//        But immutability is broken because a subclass instance can still be treated as a Money object but behave mutably.
+
+        Money money = new MutableMoney(100);
+        System.out.println(money.getAmount());  // 100
+
+        ((MutableMoney) money).setAmount(500);
+
+        System.out.println(money.getAmount());  // 500
+//        From the outside world, your supposedly immutable Money object is now mutable because a child class changed its behavior.
