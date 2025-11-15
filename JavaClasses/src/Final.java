2a4c682006d869cca1e1cd1a9de684a99346941f
shishir474 <shishirsinghvaranasi@gmail.com>
2025-11-15 22:07:35 +0530

    pojo classes

--- JavaClasses/src/Final.java
+++ JavaClasses/src/Final.java
@@ -1,2 +1,10 @@
-public class Final {
+public final class Final {
+    // Final class cannot be inherited.
+    // So if you have a usecase where you dont want your class to be inherited, just declare it as final
+
+}
+
+// not allowed -  compilation error ( cannot inherit final class )
+public class TestClass extends Final{
+
 }
--- JavaClasses/src/POJO.java
+++ JavaClasses/src/POJO.java
@@ -1,2 +1,30 @@
-public class POJO {
+public class StudentPOJOSample {
+    // POJO stands for "Plain Old Java Object"
+    // contains variables and its getters & setter methods
+    // class should be public
+    // Contains private fields with public getters/setters (not mandatory, but recommended & standard practise)
+    // It should not extend any class or implement any interface
+    // No annotations should be used like @Table, @Entity, @Id etc
+
+    String Name;
+    private int rollNumber;
+    protected String address;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "id = " + rollNumber +
+                ", name = " + name +
+                ", address = " + address +
+                '}';
+
+    }
 }
