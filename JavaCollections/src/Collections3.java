a433b402b0e68b5fa7a49dc71774aec186789f89
shishir474 <shishirsinghvaranasi@gmail.com>
2025-11-20 18:02:47 +0530

    completed ArrayList | CopyOnWriteArrayList(thread safe version)

--- JavaCollections/src/Collections3.java
+++ JavaCollections/src/Collections3.java
@@ -1,6 +1,11 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.function.UnaryOperator;
 
 public class Collections3 {
 //    Deque & List
@@ -70,6 +75,7 @@ public class Collections3 {
             while(!dq.isEmpty()){
                 System.out.println(dq.removeFirst());
             }
+
 
         }
     }
@@ -147,5 +153,211 @@ public class Collections3 {
 
 
 
+//    List
+//    List is a ordered collection of objects, in which duplicate values can be stored
+
+//    How does it differ from Queue?
+//    - Queue is also a collectino of objects, but in queue insertion/removal/access can only happen at start or end of the queue.
+//    - While in list, data can be inserted, removed, or access from anywhere.
+
+//Methods available in List Interface
+//    - all the methods from Collection interface + new Methods defined in the List interface
+//
+//    Collection methods as explained previously
+//    1. int size()       -- reutrns the total no of elements in the collection
+//    2. boolean isEmpty()
+//    3. boolean contains(E e)
+//    4. toArray()   -- to convert collection into an Array
+//    5. add(E element) -- appends element at the end of the collection
+//    6. remove(E element) -- removes the first occurrence of the element from the list
+//    6. addAll(Collection c): appends all the elements of the specified collection in the end of the list.
+//    7. removeAll(Collection c): Removes all the elements from the collections, which are present in the collection passed in the parameter.
+//    8. clear()
+//    9. equals()
+//    10. stream() and parallelStream()
+//    11. iterator()  - As iterable interface was added in java 1.5, so before this we used to use this method to iterate over the collection and still can be used
+
+//New methods defined in the List interface:
+
+//    1. add(int index, E element) - inserts the element at the specified position in the list by shifting the element and subsequent elements to right.
+//    2. addAll(int index, Collection c)
+//    3. sort(Comparator c) - sorts the list based on the Comparator
+//    4. get(int index)   -- returns the element at the specifed position in the list
+//    5. set(int index, Element e)  -- replaces the element at the specified position
+//    6. remove(int index) -- removes the element at the specified position and shifts subsequent elements to the left.
+//    7. indexOf(Object o) -- returns the index of first occurence of the specified element in the list.
+//                        -1 if the list does not contain the element.
+//    8. lastIndexOf(Object o) -- returns the index of last occurence
+//    9. ListIterator<E> listIterator() -- listIterator returns the object of ListIterator
+//                                        -- ListIterator(interface) extends from the Iterator(interface)
+//                                    -- it has all the methods from the Iterator interface
+//                                        -- hasNext()
+//                                        -- next()       - returns the next element in the iteration
+//                                        -- remove()    - returns the last element returned by the iterator
+//
+//
+//                                    -- New methods which are introduced in ListIterator, which helps in backward iteration
+//                                        -- hasPrevious()
+//                                        -- E previous() -- returns the prev element and move the pointer position backward
+//                                        -- int nextIndex() -- returns the index of next element (return size of the list, if at the end of the list)
+//                                        -- int previousIndex() -- returns the index of prev element (return -1 if at the beginning of the list)
+//                                        -- set(E e)
+//
+//
+//    10. List<E> subList(int fromIndex, int toIndex)
+//                -- fromIndex - inclusive
+//                -- toIndex - exclusive
+//        if fromIndex == toIndex, return sublist is empty
+//        Any changes in sublist, will change in main list also and vice-versa
+
+
+    class Main {
+        public static void main(String[] args) {
+            List<Integer> list1 = new ArrayList<>();
+            list1.add(100);
+            list1.add(200);
+            list1.add(300);
+            list1.add(0, 50);
+
+            System.out.println("list1");
+            list1.forEach(val -> System.out.println(val));
+
+            List<Integer> list2 = new ArrayList<>();
+            list2.add(400);
+            list2.add(500);
+            list2.add(600);
+
+            list1.addAll(list2);
+
+            System.out.println("list1 after addAll()");
+            list1.forEach(val -> System.out.println(val));
+
+            // replaceAll(UnaryOperator op)
+            // UnaryOperator is a functional interface (which means it has one abstract method and we're gonna provide its implementation via lambda expressions)
+            list1.replaceAll(val -> val * -1);
+
+            // Why this does not work list1.forEach(val -> val * -1) ?
+            // replaceAll() expects a UnaryOperator<T> whereas forEach() expects a Consumer<T>
+            // A UnaryOperator<T> takes a value and returns a value of the same type
+            // T apply(T t); // MUST return something
+
+            // A Consumer does not return anything
+            // void accept(T t); // MUST return void
+
+            // You can use forEach to print values. But if you want to mutate the list using forEach --> You cannot
+            // forEach() is not meant for modifying list elements
+
+            System.out.println("list1 after replaceAll()");
+            list1.forEach(val -> System.out.println(val));
+
+            System.out.println("list1 after sort() in asc order");
+            list1.sort((val1, val2) -> val1 - val2);
+            list1.forEach(val -> System.out.println(val));
+
+            // NOTE: passing compartor in list.sort() is manadatory
+            System.out.println("get() element at 2nd index: " + list1.get(2));
+            // throws IndexOutOfBoundsException if idx < 0 || idx >= list1.size()
+
+            // You cannot access elements using [] in list
+            // System.out.println("get() element at 2nd index: " + list1[2]);
+
+            System.out.println("list1 after set()");
+            list1.set(2, -10000);
+            list1.forEach(val -> System.out.println(val));
+
+            // remove() has 2 flavours / overloaded methods
+            // remove(int index)
+            // remove(Object o)
+            System.out.println("list1 after remove(int index)");
+            list1.remove(2);
+            list1.forEach(val -> System.out.println(val));
+
+            // remove -300
+            System.out.println("list1 after remove(Object o) ");
+            // list1.remove(-300);
+            // the above line does not work bcoz literal -300 is a primitive int, so the compiler chooses remove(int index) and thus tries to remove the element at index -300, which is invalid -> IndexOutOfBoundsException
+            list1.remove(Integer.valueOf(-300));
+            // this removes the first Occurence of the object
+
+            list1.forEach(val -> System.out.println(val));
+
+            System.out.println("indexOf(Object o): " + list1.indexOf(-100));
+            System.out.println("indexOf(Object o): " + list1.indexOf(-1000));
+            // returns -1 if the element is not present, else returns the first occurence index
+
+            System.out.println("indexOf(Object o): " + list1.lastIndexOf(-100));
+
+            ListIterator<Integer> listIterator =  list1.listIterator();
+
+            while(listIterator.hasNext()){
+                System.out.println("Traversing forward: " + " Next index: " + listIterator.nextIndex() + " Previous index: " + listIterator.previousIndex() + " value " + listIterator.next());
+            }
+
+            while(listIterator.hasPrevious()){
+                System.out.println("Traversing backward: " + " Next index: " + listIterator.nextIndex() + " Previous index: " + listIterator.previousIndex() + " value " + listIterator.previous());
+            }
+
+
+            System.out.println("sublist [1,4) :");
+            List<Integer> subList = list1.subList(1,4);
+            subList.forEach(val -> System.out.println(val));
+            subList.set(0, 100);
+            System.out.println("sublist after set() :");
+            subList.forEach(val -> System.out.println(val));
+            System.out.println("orig list after set() :");
+            list1.forEach(val -> System.out.println(val));
+            // Hence subList() just provides a view on top of orig list. Any changes made to origList will be reflected in subList and vice-versa.
+
+            ArrayList
+                    Integer
+        }
+    }
+
+//    Time Complexity:
+//    Amortized O(1): when inserting element at the end of the list. If the list is full and we try to insert an element at end, then a new ArrayList is created in the background, elements from the orig list are copied into the new list and then the new element is appended.
+//        This is an O(N) operation, but this happens very infrequently.
+//
+//        When inserting an element at the specified index, it requires shifting the elements -- this is also O(N)
+//
+//    Deletion:  -- has 2 flavours remove(int index)
+//                remove(Object o) --> removes first Occurence of Object o,
+//        O(n), we have to shift all the values
+//
+//    Search
+//        O(n)
+//
+//    Space Complexity:
+//        O(n)
+
+                    isThreadSafe        Maintains Order         Duplicates allowed          Nulls allowed
+    ArrayList           No                  Yes                     Yes                         Yes
+
+//    Thread safe version of ArrayList - CopyOnWriteArrayList
+
+ import java.util.*;
+ import java.util.concurrent.*;
+
+
+    class Main {
+        public static void main(String[] args) {
+            List<Integer> list1 = new CopyOnWriteArrayList<>();
+            list1.add(100);
+            System.out.println(list1.get(0));
+            System.out.println(list1.indexOf(100));
+            System.out.println(list1.lastIndexOf(100));
+            System.out.println(list1.indexOf(1000));
+            System.out.println(list1.contains(100));
+            list1.remove(Integer.valueOf(100));
+            System.out.println(list1.size());
+
+            System.out.println(Integer.valueOf(100));   // allowed
+            System.out.println(Integer.valueOf("123")); // allowed -- this string can be parsed
+            // System.out.println(Integer.valueOf("hello"));    // invalid throws NumberFormatException bcoz of failure to parse the string
+
+
+        }
+    }
 
 }
+
+
